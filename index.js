e84d46be67da41ac156eea7f86b638fbecd93bc5
Harold Davidson <harold@thinkhappymedium.com>
2021-11-29 22:36:37 -0800

    remove bcrypt and add bcryptjs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const importData = require('./data.json');
-//const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 app.use(express.json());
 
@@ -21,36 +21,34 @@ app.get("/coders", (req, res) => {
     res.send(importData);
 });
 
+app.get('/users', (req, res) => {
+    res.json(users)
+});
 
-
-// app.get('/users', (req, res) => {
-//     res.json(users)
-// });
-
-// app.post('/users', async (req, res) => {
-//     try {
-//         const hashedPassword = await bcrypt.hash(req.body.password, 10);
-//         const user = { name: req.body.name, password: hashedPassword }
-//         users.push(user);
-//         res.status(201).send();
-//     } catch {
-//         res.status(500).send()
-//     }
+app.post('/users', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = { name: req.body.name, password: hashedPassword }
+        users.push(user);
+        res.status(201).send();
+    } catch {
+        res.status(500).send()
+    }
     
-// });
+});
 
-// app.post('/users/login', async (req, res) => {
-//     const user = users.find(user => user.name = req.body.name)
-//     if (user == null) {
-//         return res.status(400).send('Cannot find user')
-//     }    
-//     try {
-//         if(await bcrypt.compare(req.body.password, user.password)) {
-//             res.send('Success');
-//         } else {
-//             res.send('Invalid Password');
-//         }
-//     } catch {
-//         res.status(500).send();
-//     }
-// });
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name = req.body.name)
+    if (user == null) {
+        return res.status(400).send('Cannot find user')
+    }    
+    try {
+        if(await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Success');
+        } else {
+            res.send('Invalid Password');
+        }
+    } catch {
+        res.status(500).send();
+    }
+});
